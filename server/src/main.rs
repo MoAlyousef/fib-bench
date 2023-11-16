a5ed48ee0737bbe42428ea25c98bf38ffde89793
Mohammed Alyousef <may642_2000@hotmail.com>
2023-11-16 16:06:27 +0300

    update server

--- server/src/main.rs
+++ server/src/main.rs
@@ -11,13 +11,13 @@ fn fibonacci(n: u32) -> u32 {
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     println!("Listening on 127.0.0.1:{}", args[1]);
-    let mut server = Server::new(args[1].parse().unwrap(), &args[2]);
+    let mut server = Server::new(args[1].parse().unwrap());
     server.route(Method::Post, "/", |rq| {
         let mut content = String::new();
         rq.as_reader().read_to_string(&mut content).unwrap();
         let n = fibonacci(content.parse().unwrap());
         Response::from_string(n.to_string()).boxed()
     });
-    server.static_serve(true);
+    server.serve_dir(&args[2]);
     server.serve();
 }
